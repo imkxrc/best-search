4ac088f11c8f18b9d4e004fcf0e267ca1d27db84
‘Yongsheng Huang <113082408@qq.com>
2022-06-09 14:54:26 +0800

    commit code

--- src/router/index.js
+++ src/router/index.js
@@ -2,18 +2,22 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 
 Vue.use(VueRouter)
-const Index = () => import('@/components/HelloWorld')
 const searchList = () => import('@/components/search-list.vue')
 const SearchIndex = () => import('@/components/searchIndex')
 
-const routes = [
-    { path: '/', component: SearchIndex },
-    { path: '/search/:searchKey', component: searchList },
- 
+const routes = [{
+        path: '/',
+        component: SearchIndex
+    },
+    {
+        path: '/search/:searchKey',
+        component: searchList
+    },
+
 ]
 
 const router = new VueRouter({
-    mode: 'history',//可写可不写
+    mode: 'history', //可写可不写
     base: process.env.BASE_URL,
     routes
 })
