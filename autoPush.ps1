632b5348d9dce90c16d94f85fac792ab4d8a463e
‘Yongsheng Huang <113082408@qq.com>
2022-12-04 14:44:57 +0800

    ==> 2022-12-04 14锛歮44锛歴57锛歠521 commit <==

--- autoPush.ps1
+++ autoPush.ps1
@@ -3,10 +3,10 @@ Function Auto-push-git {
  $now = Get-Date # 获取时间日期对象. 对!PowerShell是面向对象的
  $msg = "==> " + $now.ToString("yyyy-MM-dd HH：mm：ss：ffff") + " commit <=="# 提交的消息
  Write-Output $msg >> .\gitpush.log # 追加输出到 日志文件gitpush.log
- git status >> .\gitpush.log # 将代码的相关信息追加输出到日志,下面几行也是
+ git status >> .\gitpush.log # 将代码的相关信息追加输出到日志,下面几行也是 -u origin master
  git add . >> .\gitpush.log
  git commit -m $msg >> .\gitpush.log
- git push -u origin master >> .\gitpush.log
+ git push >> .\gitpush.log
 }
 # 上面实际上定义了一个函数
 Auto-push-git
