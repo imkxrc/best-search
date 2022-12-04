5d2305fe8c091858ab9f5dd5adc385ac754255aa
‘Yongsheng Huang <113082408@qq.com>
2022-12-04 14:50:25 +0800

    ==> 2022-12-04 14:50:25:4998 commit <==

--- autoPush.ps1
+++ autoPush.ps1
@@ -1,7 +1,7 @@
 Function Auto-push-git {
  Set-Location D:\study-project\vue3\best-search-gitee # 绝对路径 切换到你要推送的本地仓库地址
  $now = Get-Date # 获取时间日期对象. 对!PowerShell是面向对象的
- $msg = "==> " + $now.ToString("yyyy-MM-dd HH：mm：ss：ffff") + " commit <=="# 提交的消息
+ $msg = "==> " + $now.ToString("yyyy-MM-dd HH:mm:ss:ffff") + " commit <=="# 提交的消息
  Write-Output $msg >> .\gitpush.log # 追加输出到 日志文件gitpush.log
  git status >> .\gitpush.log # 将代码的相关信息追加输出到日志,下面几行也是 -u origin master
  git add . >> .\gitpush.log
